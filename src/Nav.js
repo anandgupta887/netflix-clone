262736c8772abdd6d9e54b9a86694ef0865ff9e1
Anand <anandgupta887@yahoo.com>
2020-08-27 13:13:49 +0530

    added navbar

---
+++ src/Nav.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+        alt="Neflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcStZLAPYXX9lnfvA0Avz3WU8E-rsXfVo8Vwrw&usqp=CAU"
+        alt="Avatar Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
